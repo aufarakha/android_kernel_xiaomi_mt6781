00614e9325c1183f6ede3f471c914f15e3ff596f
aufarakha <aufarakha434@gmail.com>
2026-01-28 05:55:20 +0700

    Add KSUN

--- fs/namespace.c
+++ fs/namespace.c
@@ -1700,6 +1700,8 @@ static int can_umount(const struct path *path, int flags)
 {
 	struct mount *mnt = real_mount(path->mnt);
 
+	if (flags & ~(MNT_FORCE | MNT_DETACH | MNT_EXPIRE | UMOUNT_NOFOLLOW))
+		return -EINVAL;
 	if (!may_mount())
 		return -EPERM;
 	if (path->dentry != path->mnt->mnt_root)
@@ -1713,7 +1715,6 @@ static int can_umount(const struct path *path, int flags)
 	return 0;
 }
 
-// caller is responsible for flags being sane
 int path_umount(struct path *path, int flags)
 {
 	struct mount *mnt = real_mount(path->mnt);
